b52f18ca0fb6501a09e7c65e51344faf4a2d137b
Louis Garcia <louis@mediagarcia.com>
2025-12-17 13:31:16 -0600

    Force rebuild: Add timestamp comment to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,4 @@
+// About page - Updated Dec 2025
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
